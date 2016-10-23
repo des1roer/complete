93682e8d2095c9957116699638e58aebdaff2e31
des <des@DESKTOP-Q9JT0H1>
2016-10-23 09:51:44 +0500

    3hd

--- php/classSave.php
+++ php/classSave.php
@@ -3,14 +3,29 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-class Save {
+class SaveClass {
+    private $log = false;
 
-// данные (свойства):
-    var $param;
+    private function Log($txt, $method) {
+        var_dump($txt);
+        file_put_contents(__DIR__ . '/logs/' . date('Y-m-d') . '_log.log', "[$method][".date('H:i:s')."]" . PHP_EOL . $txt . PHP_EOL, FILE_APPEND);
+    }
 
-// методы:
-    function Save() {
-        var_dump($_POST);
+    public function setLog($log) {
+        $this->log = $log;
+    }
+
+    public function Save($param) {
+        $param = array_filter($param);
+      //  $this->Send($param);
+         var_dump($this->log);
+    }
+
+    public function Send($param) {
+        //$param = array_filter($param);
+        //var_dump($param);
+        if ($this->log)
+            $this->Log(json_encode($param), __FUNCTION__);
     }
 
 }
--- php/index.php
+++ php/index.php
@@ -1,4 +1,20 @@
 <?php
 include_once 'classSave.php';
 
-$save = new Save($_POST);
+class SendClass extends SaveClass
+{
+    public function Save($param) {
+        $param = array_filter($param);
+        $this->Send($param);
+        // var_dump($this->log);
+    }
+}
+
+$param = $_POST;
+$param['log'] = true;
+
+$save = new SendClass();
+$save->setLog(TRUE);
+$save->Save($param);
+
+//$save->Send($param);
